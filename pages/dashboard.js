55e05824a1ff0909b207acae5060c4ba112637c1
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2023-05-28 21:48:36 +0600

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,9 +2,25 @@ import Sidebar from "@/components/Sidebar";
 import UnAuthenticated from "@/components/UnAuthenticated";
 import React, { useEffect, useState } from "react";
 import styles from "../styles/dashboard.module.css";
+import { supabase } from "@/lib/client";
 
 const Dashboard = () => {
   let [user, setUser] = useState(null);
+  const [cashLedger, setcashLedger] = useState([]);
+
+  async function fetchCashLedger() {
+    let { data, error } = await supabase.from("cash_ledger").select("*");
+    if (data) {
+      setcashLedger(data);
+    }
+    if (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCashLedger();
+  }, []);
 
   useEffect(() => {
     let isLoggedIn = JSON.parse(localStorage.getItem("user")); // should be user
@@ -15,33 +31,37 @@ const Dashboard = () => {
     return <UnAuthenticated />;
   } else {
     return (
-      <>
-      <Sidebar />
-      <div className={styles["page"]}>
-        {/* {JSON.stringify(user)} */}
-        {/* cart design start here */}
-      <div className={styles["cart"]}>
+      <div>
+        <Sidebar />
+        <div className={styles["page"]}>
+          {/* {JSON.stringify(user)} */}
+          {/* cart design start here */}
+          <div className={styles["cart"]}>
+          
+            <div className={styles["box1"]}>
+              {fetchCashLedger && (
+                <div>
+                  {cashLedger.length > 0 && (
+                    <p>Cash-{cashLedger[cashLedger.length - 1].balance}</p>
+                  )}
+                </div>
+              )}
+            </div>
 
-      <div className={styles["box1"]}>
-      <p>Hello</p>
-      </div>
+            <div className={styles["box2"]}>
+              <p></p>
+            </div>
 
-      <div className={styles["box2"]}>
-      <p>kemon</p>
-      </div>
+            <div className={styles["box3"]}>
+              <p></p>
+            </div>
 
-      <div className={styles["box3"]}>
-      <p>acho</p>
+            <div className={styles["box4"]}>
+              <p></p>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className={styles["box4"]}>
-      <p>tumi</p> 
-      </div>
-
-      </div>
-        
-      </div>
-      </>
     );
   }
 };
